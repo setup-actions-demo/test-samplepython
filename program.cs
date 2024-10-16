eecf0a36e568440e1040391a5f563269d40b0ad7
priya-kinthali <priya-kinthali@github.com>
2024-10-16 19:43:38 +0530

    initial commit

---
+++ program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
